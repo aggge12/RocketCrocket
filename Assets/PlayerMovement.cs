81285016079bb35dc17a35632d4dbec8f67709f7
aggge12 <axel.lundberg.gbg@hotmail.com>
2020-10-28 22:38:19 +0100

    first

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int turnSpeed;
+    public int speed;
+    public Vector3 rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+      turnSpeed = 40;
+      speed = 20;
+    }
+
+    void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        this.rotation = new Vector3(0, 0, moveHorizontal * turnSpeed * Time.deltaTime);
+        this.transform.Rotate(this.rotation,Space.Self);
+
+        Vector3 position = transform.position;
+
+        transform.position -= transform.up * moveVertical * speed * Time.deltaTime;
+      }
+}
