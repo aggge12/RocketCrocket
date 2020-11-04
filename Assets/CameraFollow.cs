04f4879728730cd370a0762dcee0046e02c700a8
aggge12 <axel.lundberg.gbg@hotmail.com>
2020-11-04 21:24:07 +0100

    yes

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public GameObject target;
+    public GameObject player;
+
     // Start is called before the first frame update
+    public float lastVelocity;
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        transform.position = player.transform.position + new Vector3(0, 2, -8);
+        lastVelocity = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        PlayerMovement control = player.GetComponent<PlayerMovement>();
+
+        float b = (float)0.01 * (float)control.velocity;
+        
+
+        if (b > lastVelocity){
+            transform.position -= transform.forward * b * Time.deltaTime;
+        } else if (b < lastVelocity){
+            transform.position -= transform.forward * -b * Time.deltaTime;
+        }
+
+        lastVelocity = b;
         
     }
 
      void LateUpdate() {
-        transform.LookAt(target.transform);
+        //transform.LookAt(target.transform);
     }
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,14 +7,19 @@ public class PlayerMovement : MonoBehaviour
     public int turnSpeed;
     public int speed;
     public Vector3 rotation;
+    public float velocity;
     // Start is called before the first frame update
     void Start()
     {
-      turnSpeed = 40;
-      speed = 20;
+      turnSpeed = 60;
+      speed = 15;
+      velocity = 0;
     }
 
     void Update () {
+
+        float tryjump = Input.GetKeyDown("space") ? 1 : 0;
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
@@ -23,6 +28,16 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 position = transform.position;
 
-        transform.position -= transform.up * moveVertical * speed * Time.deltaTime;
+        transform.position -= transform.up * ((moveVertical * speed) + velocity) * Time.deltaTime;
+        transform.position -= transform.forward * - ( tryjump * 1000 ) * Time.deltaTime;
+        /////// JUMP
+
+        /////// VELOCITY
+        
+        if (moveVertical > 0 && velocity < 100){
+          velocity += (float)0.25;
+        } else if (velocity > 0){
+          velocity -= (float)0.25;
+        }
       }
 }
